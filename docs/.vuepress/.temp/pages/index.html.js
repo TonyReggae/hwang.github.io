b6cf58fd6aec669196546fedacd1cf291dcc388c
Kun <superman@example.com>
2025-02-20 17:09:37 +0800

    update

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,3 +1,3 @@
 import comp from "E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello World!\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello World!\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1740039868000,\"contributors\":[{\"name\":\"Kun\",\"username\":\"Kun\",\"email\":\"superman@example.com\",\"commits\":1,\"url\":\"https://github.com/Kun\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,15 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     bundler: viteBundler(),
     theme: defaultTheme(),
+    // dest: '../hwang.github.io.pages/',
+    // public: './',
+    theme: defaultTheme({
+        // 默认主题配置
+        navbar: [
+          {
+            text: '首页',
+            link: '/',
+          },
+        ],
+      }),
 })
