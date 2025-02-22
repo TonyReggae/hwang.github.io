f268e72d24db194b06cb836149862bb971c67bc4
Kun <superman@example.com>
2025-02-22 22:15:53 +0800

    update

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -1,4 +1,4 @@
-export const redirects = JSON.parse("{\"/notes/demo/bar.html\":\"/demo/v28embst/\",\"/notes/demo/foo.html\":\"/demo/sdralx37/\",\"/notes/demo/\":\"/demo/\",\"/notes/nio/\":\"/nio/\",\"/en/notes/demo/bar.html\":\"/en/demo/lo63bxxx/\",\"/en/notes/demo/foo.html\":\"/en/demo/33dg5uy5/\",\"/en/notes/demo/\":\"/en/demo/\",\"/notes/nio/NIO%E4%B8%8ESocket%E7%BC%96%E7%A8%8B%E6%8A%80%E6%9C%AF/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.3_CharBuffer.html\":\"/nio/unpl3i2c/\",\"/notes/nio/NIO%E4%B8%8ESocket%E7%BC%96%E7%A8%8B%E6%8A%80%E6%9C%AF/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.4_Channel.html\":\"/nio/3fzoxgbn/\",\"/notes/nio/NIO%E4%B8%8ESocket%E7%BC%96%E7%A8%8B%E6%8A%80%E6%9C%AF/2.%E9%80%9A%E9%81%93/2.1_Channel.html\":\"/nio/xmtqbxjy/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.1_Buffer.html\":\"/nio/87wfm477/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.2_ByteBuffer.html\":\"/nio/bjn5txqe/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.3_CharBuffer.html\":\"/nio/unpl3i2c/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.4_Channel.html\":\"/nio/3fzoxgbn/\",\"/notes/nio/2.%E9%80%9A%E9%81%93/2.1_Channel.html\":\"/nio/xmtqbxjy/\"}")
+export const redirects = JSON.parse("{\"/notes/demo/bar.html\":\"/demo/v28embst/\",\"/notes/demo/foo.html\":\"/demo/sdralx37/\",\"/notes/demo/\":\"/demo/\",\"/notes/nio/\":\"/nio/\",\"/en/notes/demo/bar.html\":\"/en/demo/lo63bxxx/\",\"/en/notes/demo/foo.html\":\"/en/demo/33dg5uy5/\",\"/en/notes/demo/\":\"/en/demo/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.1_Buffer.html\":\"/nio/87wfm477/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.2_ByteBuffer.html\":\"/nio/bjn5txqe/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.3_CharBuffer.html\":\"/nio/unpl3i2c/\",\"/notes/nio/1.%E7%BC%93%E5%86%B2%E5%8C%BA/1.4_Channel.html\":\"/nio/3fzoxgbn/\",\"/notes/nio/2.%E9%80%9A%E9%81%93/2.1_Channel.html\":\"/nio/xmtqbxjy/\"}")
 
 export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
@@ -10,6 +10,8 @@ export const routes = Object.fromEntries([
   ["/en/demo/lo63bxxx/", { loader: () => import(/* webpackChunkName: "en_demo_lo63bxxx_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/en/demo/lo63bxxx/index.html.js"), meta: {"title":"bar"} }],
   ["/en/demo/33dg5uy5/", { loader: () => import(/* webpackChunkName: "en_demo_33dg5uy5_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/en/demo/33dg5uy5/index.html.js"), meta: {"title":"foo"} }],
   ["/en/demo/", { loader: () => import(/* webpackChunkName: "en_demo_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/en/demo/index.html.js"), meta: {"title":"Demo"} }],
+  ["/nio/87wfm477/", { loader: () => import(/* webpackChunkName: "nio_87wfm477_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/87wfm477/index.html.js"), meta: {"title":"1.1 Buffer"} }],
+  ["/nio/bjn5txqe/", { loader: () => import(/* webpackChunkName: "nio_bjn5txqe_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/bjn5txqe/index.html.js"), meta: {"title":"1.2 ByteBuffer"} }],
   ["/nio/unpl3i2c/", { loader: () => import(/* webpackChunkName: "nio_unpl3i2c_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/unpl3i2c/index.html.js"), meta: {"title":"CharBuffer"} }],
   ["/nio/3fzoxgbn/", { loader: () => import(/* webpackChunkName: "nio_3fzoxgbn_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/3fzoxgbn/index.html.js"), meta: {"title":"Channel"} }],
   ["/nio/xmtqbxjy/", { loader: () => import(/* webpackChunkName: "nio_xmtqbxjy_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/xmtqbxjy/index.html.js"), meta: {"title":"Channel"} }],
@@ -18,40 +20,4 @@ export const routes = Object.fromEntries([
   ["/blog/archives/", { loader: () => import(/* webpackChunkName: "blog_archives_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/blog/archives/index.html.js"), meta: {"title":"归档"} }],
   ["/en/blog/", { loader: () => import(/* webpackChunkName: "en_blog_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/en/blog/index.html.js"), meta: {"title":"Blog"} }],
   ["/en/blog/archives/", { loader: () => import(/* webpackChunkName: "en_blog_archives_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/en/blog/archives/index.html.js"), meta: {"title":"Archives"} }],
-  ["/nio/87wfm477/", { loader: () => import(/* webpackChunkName: "nio_87wfm477_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/87wfm477/index.html.js"), meta: {"title":"1.1 Buffer"} }],
-  ["/nio/bjn5txqe/", { loader: () => import(/* webpackChunkName: "nio_bjn5txqe_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/bjn5txqe/index.html.js"), meta: {"title":"1.2 ByteBuffer"} }],
-  ["/nio/unpl3i2c/", { loader: () => import(/* webpackChunkName: "nio_unpl3i2c_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/unpl3i2c/index.html.js"), meta: {"title":"CharBuffer"} }],
-  ["/nio/3fzoxgbn/", { loader: () => import(/* webpackChunkName: "nio_3fzoxgbn_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/3fzoxgbn/index.html.js"), meta: {"title":"Channel"} }],
-  ["/nio/xmtqbxjy/", { loader: () => import(/* webpackChunkName: "nio_xmtqbxjy_index.html" */"E:/codes/hwang.github.io/docs/.vuepress/.temp/pages/nio/xmtqbxjy/index.html.js"), meta: {"title":"Channel"} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
-updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
